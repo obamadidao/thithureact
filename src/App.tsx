55e7c0244bd7ed292dbc8163c78958638940e6dd
obamadidao <laohetpin@gmail.com>
2025-08-15 10:04:11 +0700

    thi reac FE2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { useRoutes } from "react-router-dom";
 import "antd/dist/reset.css";
+import MainLayout from "./layouts/main";
 
 function App() {
 const routes = useRoutes([
   {
-    
+    path: "",
+    element: <MainLayout />,
+    children: [
+
+      {
+        path: "auth/register",
+      },
+      {
+        path: "auth/login",
+      }
+    ]
   }
 ]);
+return <div>[{routes}]</div>
 }
-
 export default App;
--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -2,11 +2,12 @@ import { Layout, Menu } from "antd";
 import { Header } from "antd/es/layout/layout";
 import { Link } from "react-router-dom";
 const MainLayout = () => {
+    return (
        <Layout style={{ minHeight: "100vh" }}>
       <Header>
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1">
-            <Link to="/">WEB2091</Link>
+            <Link to="/">Thi</Link>
           </Menu.Item>
           <Menu.Item key="2">
             <Link to="/">List</Link>
@@ -14,11 +15,18 @@ const MainLayout = () => {
           <Menu.Item key="3">
             <Link to="/">Add</Link>
           </Menu.Item>
+          <Menu.Item key="4">
+            <Link to="/auth/register">Register</Link>
+          </Menu.Item>
+          <Menu.Item key="5">
+            <Link to="/auth/login">Login</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <main style={{ padding: "0 100px" }}>
         <h1 style={{ textAlign: "center", margin: 10 }}>Bài Làm</h1>
       </main>
     </Layout> 
+    );
 };
 export default MainLayout;
