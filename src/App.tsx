f63ec14536f6ffa5259537ef3eea5e70f50086c4
obamadidao <laohetpin@gmail.com>
2025-08-15 09:48:12 +0700

    bat dau thi

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import { Layout, Menu } from "antd";
-import { Header } from "antd/es/layout/layout";
-import { Link } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import "antd/dist/reset.css";
 
 function App() {
-  return (
-    <Layout style={{ minHeight: "100vh" }}>
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1">
-            <Link to="/">WEB2091</Link>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <Link to="/">List</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/">Add</Link>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <main style={{ padding: "0 100px" }}>
-        <h1 style={{ textAlign: "center", margin: 10 }}>Bài Làm</h1>
-      </main>
-    </Layout>
-  );
+const routes = useRoutes([
+  {
+    
+  }
+]);
 }
 
 export default App;
